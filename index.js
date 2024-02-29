96d3d23405e49cb2f425b7b29a27229b9a5d378f
Mark Benjamin <mark@render.com>
2024-02-29 16:14:42 -0500

    Fix int port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const redis = require('redis');
 
 const app = express()
 app.set('view engine', 'ejs');
-const port = 3000 + Math.random() * 1000;
+const port = 3000 + Math.floor(Math.random() * 1000);
 console.log(`Listening on port ${port}`)
 
 app.get('/', async (req, res) => {
