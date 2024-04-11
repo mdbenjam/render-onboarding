6c86651de1d10151f8bf326079a1c928ccf9dd7d
Mark Benjamin <mark@render.com>
2024-04-11 16:55:56 -0400

    Update name 5

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', async (req, res) => {
     const data = {
         // Data provided by your application
         // Make your changes here!
-        author: "Mark 4",
+        author: "Mark 5",
         hits: await getHitCount(),
 
         // Data that your configure via Render: how to connect to managed datastores
