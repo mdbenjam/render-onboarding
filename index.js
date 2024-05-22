d63bfde692c1066ca13698411a3737e92d4e43e6
Mark Benjamin <mark@render.com>
2024-05-21 21:45:52 -0400

    Bind to ip

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const redis = require('redis');
+const { networkInterfaces } = require('os');
 
 const app = express()
 app.set('view engine', 'ejs');
 const port = 3000;
 
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+        if (net.family === familyV4Value && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
 
 app.get('/', async (req, res) => {
     const data = {
@@ -38,6 +56,6 @@ const getHitCount = async () => {
     }
 }
 
-app.listen(port, () => {
-    console.log(`Render onboarding app listening on port ${port}`)
+app.listen(port, results["en0"][0], () => {
+    console.log(`Render onboarding app listening on address ${results["en0"][0]} port ${port}`)
 })
