946108e263fc254ef2d656ebce5eb8905c10e595
Mark Benjamin <mark@render.com>
2024-05-23 12:57:24 -0400

    use ipv4

--- index.js
+++ index.js
@@ -62,6 +62,6 @@ app.listen(port, results["eth0"][0], () => {
     console.log(`Render onboarding app listening on address ${results["eth0"][0]} port ${port}`)
 })
 
-app.listen(port, "::", () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Render onboarding app listening on address :: port ${port}`)
 })
