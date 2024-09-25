aabb72d1c32a48475820f5e8d1fa416425e9e138
Mark Benjamin <mark@render.com>
2024-09-25 15:25:06 -0400

    Try to send data

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var dgram = require('dgram');
 var port = 44044;
-var dns = require('dns');
 
-dns.promises.setServers([
-  "1.1.1.1",
-]);
+// creating a client socket
+const client = dgram.createSocket('udp4')
 
-setInterval(() => {
-    dns.promises.resolve('www.google.com').then((address) => {
-        console.log('Address: ', address);
-    }).catch((err) => {
-        console.log('Error: ', err);
-    });
-}, 1000);
+//buffer msg
+const data = Buffer.from('#01\r')
+
+//sending msg
+client.send(data, 1025, '127.0.0.1', error => {
+  if (error) {
+      console.log(error)
+      client.close()
+  } else {
+      console.log('Data sent !!!')
+  }
+})
 
 
 socket = dgram.createSocket('udp4');
