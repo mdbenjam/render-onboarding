16eba03c747fc8b34382b2937f715d48bfe3374c
Mark Benjamin <mark@render.com>
2024-11-22 13:49:34 -0500

    Add endpoints with different response sizes

--- index.js
+++ index.js
@@ -7,36 +7,13 @@ const port = 3000;
 
 
 app.get('/', async (req, res) => {
-    const data = {
-        // Data provided by your application
-        // Make your changes here!
-        author: "Adam",
-        hits: await getHitCount(),
-
-        // Data that your configure via Render: how to connect to managed datastores
-        REDIS_URL: process.env.REDIS_URL,
-
-        // Data provided by Render automatically about your code
-        RENDER_GIT_COMMIT: process.env.RENDER_GIT_COMMIT,
-        RENDER_SERVICE_NAME: process.env.RENDER_SERVICE_NAME,
-        RENDER_EXTERNAL_HOSTNAME: process.env.RENDER_EXTERNAL_HOSTNAME,
-    }
-
-    res.render('index', data);
+    res.send("")
 })
 
-
-
-const getHitCount = async () => {
-    try {
-        const client = await redis.createClient({ url: process.env.REDIS_URL, retry_strategy: (o) => { return undefined } });
-        client.on('error', (err) => { console.log(err); throw new Error(err) });
-        await client.connect();
-        return client.incr("hits")
-    } catch (e) {
-        return "(Couldn't connect to datastore)";
-    }
-}
+app.get('/header', async (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send("")
+})
 
 app.listen(port, () => {
     console.log(`Render onboarding app listening on port ${port}`)
