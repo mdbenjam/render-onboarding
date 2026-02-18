7c42ebe94d20d02e6c8d0735c955160e9e03bb5e
Mark Benjamin <mark@render.com>
2026-02-17 20:23:37 -0500

    update

--- workflow/task.py
+++ workflow/task.py
@@ -46,7 +46,7 @@ def add_numbers(a: int, b: int) -> int:
 def greet(name: str) -> str:
     """Greet someone."""
     logger.info(f"Greeting {name}")
-    return f"Hello 6, {name}!"
+    return f"Hello 7, {name}!"
 
 
 @task
