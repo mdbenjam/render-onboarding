c6eac501ffab208e513748e77a3e0d6bf3e95518
Mark Benjamin <mark@render.com>
2026-08-05 16:01:26 -0400

    update tasks

--- ts-task/main.ts
+++ ts-task/main.ts
@@ -1,17 +1,17 @@
-import { task } from "@renderinc/sdk/workflows";
+import { type TaskContext, task } from "@renderinc/sdk/workflows";
 
 /**
  * Task that squares a number
  */
-const square = task({ name: "square" }, function square(n: number): number {
-  console.log(`Calculating square of ${n}`);
+const square = task({ name: "square" }, function square(_ctx: TaskContext, n: number): number {
+  console.log(`Calculating square of ${n} ${_ctx}`);
   return n * n;
 });
 
 /**
  * Task that adds the squares of two numbers
  */
-task({ name: "add_squares" }, async function add_squares(a: number, b: number): Promise<number> {
+task({ name: "add_squares" }, async function add_squares(ctx: TaskContext, a: number, b: number): Promise<number> {
   console.log(`Calculating add_squares of ${a} and ${b}`);
-  return await square(a) + await square(b);
+  return await ctx.dispatch(square, a) + await ctx.dispatch(square, b);
 });
